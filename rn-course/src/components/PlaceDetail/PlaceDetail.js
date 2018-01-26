f22bd5f22416e3b78ef043b3697e22d95fa43eb5
Ricardo Alcantara <alcantarafox@yahoo.com.br>
2018-01-26 11:38:34 -0200

    backup passado

--- rn-course/src/components/PlaceDetail/PlaceDetail.js
+++ rn-course/src/components/PlaceDetail/PlaceDetail.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Modal, View, Image, Text, Button, StyleSheet } from 'react-native';
+import { Modal, View, Image, Text, Button, StyleSheet, TouchableOpacity } from 'react-native';
+
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const placeDetail = props => {
   let modalContent = null;
@@ -18,7 +20,11 @@ const placeDetail = props => {
       <View style={styles.modalContent}>
         {modalContent}
         <View>
-          <Button title="Delete" onPress={props.onItemDeleted} color='red' />
+          <TouchableOpacity onPress={props.onItemDeleted} >
+            <View style={styles.deleteButton}>
+              <Icon size={30} name='ios-trash' color='red' />
+            </View>
+          </TouchableOpacity>
           <Button title="Close" onPress={props.onModalClosed} />
         </View>
       </View>
@@ -38,6 +44,9 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textAlign: 'center',
     fontSize: 28
+  },
+  deleteButton: {
+    alignItems: 'center'
   }
 });
 
